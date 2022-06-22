dd0b3ecd6db12d4cb215c022f7fda8da4d3a82ca
Gustavo Silva <85652089+SSilvaGustavo@users.noreply.github.com>
2022-06-22 19:15:59 -0300

    Implementing Lesson/Sidebar component

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4n4k5w68h3x01yra00r5rzf/master',
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4obg9b11mht01z7dzhd74hz/master',
   cache: new InMemoryCache(),
 })
